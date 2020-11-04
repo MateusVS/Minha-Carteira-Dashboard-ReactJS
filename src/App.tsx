8ae09d6f7e5e1984ef657bcd0e02775644db5760
MateusVS <mateusviniciussilva07@gmail.com>
2020-11-03 23:50:36 -0300

    Criacao componente Select e headerContent dinamico

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import GlobalStyles from './styles/GlobalStyles';
 
 import Layout from './components/Layout';
 import Dashboard from './pages/Dashboard';
+import List from './pages/List';
 
 import dark from './styles/themes/dark';
 import light from './styles/themes/light';
@@ -14,7 +15,7 @@ const App: React.FC = () => {
         <ThemeProvider theme={dark}>
             <GlobalStyles />
             <Layout>
-                <Dashboard />
+                <List />
             </Layout>
         </ThemeProvider>
     );
