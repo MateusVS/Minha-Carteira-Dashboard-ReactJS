d66f71d589433a8e219d342811159d434460b701
mateusvs <mateusviniciussilva07@gmail.com>
2021-02-01 22:21:47 -0300

    Criacao botao toggle abrir e fechar menu

--- src/components/Aside/index.tsx
+++ src/components/Aside/index.tsx
@@ -1,18 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-import { MdDashboard, MdArrowDownward, MdArrowUpward, MdExitToApp } from 'react-icons/md';
+import { MdDashboard, MdArrowDownward, MdArrowUpward, MdExitToApp, MdClose, MdMenu,  } from 'react-icons/md';
 
 import logoImg from '../../assets/logo.svg';
 
 import { useAuth } from '../../hooks/auth';
 
-import { Container, Header, LogoImg, Title, MenuContainer, MenuItemLink, MenuItemButton } from './styles';
+import { Container, Header, LogoImg, Title, MenuContainer, MenuItemLink, MenuItemButton, ToggleMenu } from './styles';
 
 const Aside: React.FC = () => {
+    const [ toggleMenuIsOpened, setToggleMenuIsOpened ] = useState();
     const { SignOut } = useAuth();
+
+    const handleToggleMenu = () => {
+        setToggleMenuIsOpened(!toggleMenuIsOpened);
+    }
+
     return (
-        <Container menuIsOpen={false}>
+        <Container menuIsOpen={toggleMenuIsOpened}>
             <Header>
+                <ToggleMenu onClick={handleToggleMenu}>
+                    { toggleMenuIsOpened ? <MdClose /> : <MdMenu />}
+                </ToggleMenu>
                 <LogoImg src={logoImg} alt="Logo Minha Carteira" />
                 <Title>Minha Carteira</Title>
             </Header>
--- src/components/Aside/styles.ts
+++ src/components/Aside/styles.ts
@@ -35,8 +35,7 @@ export const LogoImg = styled.img`
     height: 40px;
     width: 40px;
     @media(max-width: 600px) {
-        height: 25px;
-        width: 25px;
+        display: none;
     }
 `;
 
@@ -85,5 +84,25 @@ export const MenuItemButton = styled.button`
     > svg {
         font-size: 18px;
         margin-right: 5px;
+    }
+`;
+
+export const ToggleMenu = styled.button`
+    width: 40px;
+    height: 40px;
+    border-radius: 5px;
+    font-size: 22px;
+    background-color: ${props => props.theme.colors.warning};
+    color: ${props => props.theme.colors.white};
+    transition: opacity .3s;
+    &:hover {
+        opacity: 0.7;
+    }
+    display: none;
+
+    @media(max-width: 600px) {
+        display: flex;
+        justify-content: center;
+        align-items: center;
     }
 `;
